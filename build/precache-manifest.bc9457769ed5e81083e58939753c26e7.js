41ef4ce06864e12167448e9a204a110f51c68e70
abhi <abhijitshaw88@gmail.com>
2020-10-01 16:31:32 +0530

    build added

---
+++ build/precache-manifest.bc9457769ed5e81083e58939753c26e7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92407ee3385cf750e49d05fffa48c38f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7def6c33aadf27f4582",
+    "url": "/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "419efb55e7732e3143cd",
+    "url": "/static/css/main.57f36976.chunk.css"
+  },
+  {
+    "revision": "c7def6c33aadf27f4582",
+    "url": "/static/js/2.6ffb415e.chunk.js"
+  },
+  {
+    "revision": "5174e6c8ea76baf1082b04ccda3b5fca",
+    "url": "/static/js/2.6ffb415e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419efb55e7732e3143cd",
+    "url": "/static/js/main.447f9c55.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6c3958a0d4b5f50c0f2f58bbd7e895f3",
+    "url": "/static/media/account.6c3958a0.svg"
+  },
+  {
+    "revision": "80eb3a16a86135c91b3682c7bc2bd56d",
+    "url": "/static/media/bill.80eb3a16.svg"
+  },
+  {
+    "revision": "1f4d9eac444ad6955a000d1c275e1f2d",
+    "url": "/static/media/brick.1f4d9eac.svg"
+  },
+  {
+    "revision": "57c263325cba3d1a74e4dc76a9e5dd87",
+    "url": "/static/media/calendar.57c26332.svg"
+  },
+  {
+    "revision": "e93dd461908f63161ff73f314d59dcae",
+    "url": "/static/media/companylogo.e93dd461.jpg"
+  },
+  {
+    "revision": "e3503594fbcfaa777903acbf4f9e78cb",
+    "url": "/static/media/dashboard.e3503594.svg"
+  },
+  {
+    "revision": "9d7769d146e4fbe959f09437b54b7ea0",
+    "url": "/static/media/delivery.9d7769d1.svg"
+  },
+  {
+    "revision": "a5da92b756e1c05728b46270821db81d",
+    "url": "/static/media/pdf.a5da92b7.svg"
+  },
+  {
+    "revision": "fcf014fdb370df9798ff1cb6b1b907d2",
+    "url": "/static/media/pin.fcf014fd.svg"
+  },
+  {
+    "revision": "9c4128a52d372917a14c778006088b1e",
+    "url": "/static/media/project.9c4128a5.svg"
+  },
+  {
+    "revision": "118e43c9bb48ecd52e0165592325afe5",
+    "url": "/static/media/search.118e43c9.svg"
+  },
+  {
+    "revision": "26159e7caef40a62b2353e75e8852904",
+    "url": "/static/media/settings.26159e7c.svg"
+  },
+  {
+    "revision": "9537524b4a381bd9854f3719ac63884c",
+    "url": "/static/media/team.9537524b.svg"
+  },
+  {
+    "revision": "e20f8e9d3c44608f47426b57d3d0d071",
+    "url": "/static/media/truck.e20f8e9d.svg"
+  }
+]);
